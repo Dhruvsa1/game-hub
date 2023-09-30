1371736a98769283d1f1e60c105d774cbe7c08c5
Dhruvsai Dhulipudi <ddhruvsai@outlook.com>
2023-09-29 20:01:11 -0400

    Made the visual component of the search bar... no function yet

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,16 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
     <HStack justifyContent='space-between' padding='10px'>
-      <HStack>
+     
         <Image src={logo} boxSize="60px" />
         <Text fontSize={25} fontWeight={"bold"} fontFamily='sans-serif'>GameHub</Text>
-      </HStack>
+    
+      <SearchInput/>
       <ColorModeSwitch />
     </HStack>
   );
